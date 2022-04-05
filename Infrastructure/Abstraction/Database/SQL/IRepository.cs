07c7e780ff3bd8bda3ad3cdeafa2b7447b99011d
Olaniyan Elias <elias.akin@gmail.com>
2022-04-05 16:46:44 +0100

    repo

--- Infrastructure/Abstraction/Database/SQL/IRepository.cs
+++ Infrastructure/Abstraction/Database/SQL/IRepository.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿using Infrastructure.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Abstraction.Database.SQL {
     public interface IRepository {
+        void beginTransacion();
+        Task<bool> commit();
+        Task<bool> isExist(string sql, IEnumerable<object> q);
+        Task<QueryResult<T>> selectFromQuery<T>(string sql, IEnumerable<object> q);
     }
 }
