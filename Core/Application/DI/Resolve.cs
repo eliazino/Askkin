9357907add428e4f99c66aa1736a508f774c9680
Olaniyan Elias <elias.akin@gmail.com>
2022-04-05 13:40:52 +0100

    DI Resolve

---
+++ Core/Application/DI/Resolve.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetCore.AutoRegisterDi;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.DI {
+    public static class Resolve {
+        public static void resolve(this IServiceCollection services) {
+            services.RegisterAssemblyPublicNonGenericClasses()
+              .AsPublicImplementedInterfaces();
+        }
+    }
+}
