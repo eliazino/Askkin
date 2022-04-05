58f44ea9c24c6f492b09b4dbf4e943f49f01b01b
Olaniyan Elias <elias.akin@gmail.com>
2022-04-05 11:50:20 +0100

    interfaces

---
+++ Core/Application/Interfaces/Identity/IIdentityManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Application.Interfaces.Identity {
+    public interface IIdentityManager {
+        string message { get; }
+        bool valid { get; }
+        bool sessionValid();
+        string getJWTIdentity(Dictionary<string, string> identity, int expiry = 0);
+        T getProfile<T>();
+        string getHeaderValue(string key);
+        IDictionary<string, object> getAllHeader();
+        void loadCustomHeaders(IDictionary<string, object> header);
+        string IPAddress { get; }
+        string endPointAddress { get; }
+    }
+}
