dc6837702ce8f23b3cc473a4a1d5e03f0aa1ced9
Olaniyan Elias <elias.akin@gmail.com>
2022-04-06 15:18:37 +0100

    user entity

---
+++ Core/Model/Entities/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Model.Entities {
+    public class Users {
+        public long id { get; protected set; }
+        public string email { get; protected set; }
+        public string username { get; protected set; }
+        public string password { get; protected set; }
+        public string avatar { get; protected set; }
+        public string fullname { get; protected set; }
+        public long lastlogin { get; protected set; }
+    }
+}
