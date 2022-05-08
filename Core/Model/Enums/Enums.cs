98965b359109cd96e4ef81512cfc4bb179766312
Olaniyan Elias <elias.akin@gmail.com>
2022-05-08 20:01:43 +0100

    enums

---
+++ Core/Model/Enums/Enums.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Model.Enums {
+    public enum PostStatus {
+        ACTIVE,
+        DELETED,
+        SUSPENDED,
+        BLACKLISTED
+    }
+
+    public enum PostReaction {
+        LIKE,
+        LOVE,
+        HELPFUL,
+        MISLEADING, 
+    }
+    public enum Privilege {
+        CREATOR,
+        MODERATOR
+    }
+
+    public enum ReportClass {
+        MISLEADING,
+        HATEFUL
+    }
+
+    public enum ReportStatus {
+        PENDING,
+        INVESTIGATION
+    }
+    public enum NotificationType {
+        COMMENT,
+        SECURIT
+    }
+}
